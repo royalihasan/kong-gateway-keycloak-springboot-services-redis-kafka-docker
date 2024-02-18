a3c4f6f15775a54fce6d4cd1caf9d304b96ba5fb
royalihasan <royalihasan0987@gmail.com>
2024-02-18 19:32:23 +0500

    Prepare service alpha for auth and authorization => [added]

--- micro-service-alpha/src/main/java/org/integrationgateways/microservicealpha/controllers/TestController.java
+++ micro-service-alpha/src/main/java/org/integrationgateways/microservicealpha/controllers/TestController.java
@@ -1,14 +1,22 @@
 package org.integrationgateways.microservicealpha.controllers;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/serve")
+@RequestMapping()
 public class TestController {
-    @GetMapping("")
-    public String test() {
-        return "Hello";
+
+    @GetMapping("/home")
+    public String index(@AuthenticationPrincipal Jwt jwt) {
+        return String.format("Hello, %s!", jwt.getClaimAsString("preferred_username"));
+    }
+
+    @GetMapping("/protected/premium")
+    public String premium(@AuthenticationPrincipal Jwt jwt) {
+        return String.format("Hello, %s!", jwt.getClaimAsString("preferred_username"));
     }
 }
--- micro-service-alpha/src/main/java/org/integrationgateways/microservicealpha/security/SecurityConfig.java
+++ micro-service-alpha/src/main/java/org/integrationgateways/microservicealpha/security/SecurityConfig.java
@@ -1,19 +1,23 @@
 package org.integrationgateways.microservicealpha.security;
 
+
+import java.io.IOException;
+
 import org.keycloak.adapters.authorization.integration.jakarta.ServletPolicyEnforcerFilter;
 import org.keycloak.adapters.authorization.spi.ConfigurationResolver;
 import org.keycloak.adapters.authorization.spi.HttpRequest;
 import org.keycloak.representations.adapters.config.PolicyEnforcerConfig;
 import org.keycloak.util.JsonSerialization;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -21,31 +25,38 @@ import java.io.IOException;
 
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)
-            throws Exception {
-        http.csrf(t -> t.disable());
-        http.addFilterAfter(createPolicyEnforcerFilter(),
-                BearerTokenAuthenticationFilter.class);
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    String jwkSetUri;
 
-        http.sessionManagement(
-                t -> t.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        );
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .addFilterAfter(createPolicyEnforcerFilter(), BearerTokenAuthenticationFilter.class);
         return http.build();
     }
 
     private ServletPolicyEnforcerFilter createPolicyEnforcerFilter() {
+        PolicyEnforcerConfig config;
+
+        try {
+            config = JsonSerialization.readValue(getClass().getResourceAsStream("/policy-enforcer.json"), PolicyEnforcerConfig.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         return new ServletPolicyEnforcerFilter(new ConfigurationResolver() {
             @Override
             public PolicyEnforcerConfig resolve(HttpRequest request) {
-                try {
-                    return JsonSerialization.
-                            readValue(getClass().getResourceAsStream("/policy-enforcer.json"),
-                                    PolicyEnforcerConfig.class);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                return config;
             }
         });
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
     }
 }
